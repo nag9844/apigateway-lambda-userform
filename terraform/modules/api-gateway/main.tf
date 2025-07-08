303de48c7b59e11fd1cb5d4fa6c735153881ef7d
nag9844 <96878041+nag9844@users.noreply.github.com>
2025-07-08 15:02:03 +0530

    update

--- terraform/modules/api-gateway/main.tf
+++ terraform/modules/api-gateway/main.tf
@@ -41,7 +41,7 @@ resource "aws_api_gateway_integration" "contact_post_integration" {
 
   integration_http_method = "POST"
   type                   = "AWS_PROXY"
-  uri                    = var.lambda_function_arn
+  uri                    = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_function_arn}/invocations"
 }
 
 # API Gateway Integration - OPTIONS (for CORS)
